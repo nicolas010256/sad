a32ada0672c98df9eb06b011dab37804e43dccb0
Nicolas <nicolas010256@gmail.com>
2018-11-19 18:00:49 -0200

    Add BancaController

---
+++ sad/src/main/java/controller/BancaController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import dao.BancaDAO;
+import dao.BancaDAOImpl;
+import dao.exceptions.BancaDAOException;
+import entity.Banca;
+import entity.Trabalho;
+
+public class BancaController {
+    private BancaDAO dao = new BancaDAOImpl();
+
+    public void add(Banca banca) {
+        try {
+            dao.add(banca);
+        } catch (BancaDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Banca get(long id) {
+        Banca banca = null;
+        try {
+            banca = dao.get(id);
+        } catch (BancaDAOException e) {
+            e.printStackTrace();
+        }
+        return banca;
+    }
+
+    public Banca getByTrabalho(Trabalho trabalho) {
+        Banca banca = null;
+        try {
+            banca = dao.getByTrabalho(trabalho);
+        } catch (BancaDAOException e) {
+            e.printStackTrace();
+        }
+        return banca;
+    }
+
+    public void update(Banca banca) {
+        try {
+            dao.update(banca);
+        } catch (BancaDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remove(Banca banca) {
+        try {
+            dao.remove(banca);
+        } catch (BancaDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
