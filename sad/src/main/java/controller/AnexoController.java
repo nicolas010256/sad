dcfb88d5fba7f526d29fcf9bfa72ae16e561e295
Nicolas <nicolas010256@gmail.com>
2018-11-18 22:15:42 -0200

    Update AnexoController

--- sad/src/main/java/controller/AnexoController.java
+++ sad/src/main/java/controller/AnexoController.java
@@ -1,7 +1,46 @@
 package controller;
 
+import java.util.List;
 
+import dao.AnexoDAO;
+import dao.AnexoDAOImpl;
+import dao.exceptions.AnexoDAOException;
+import entity.Anexo;
+import entity.Mensagem;
 
 public class AnexoController {
-    
+    AnexoDAO dao = new AnexoDAOImpl();
+
+    public void add(Anexo anexo) {
+        try {
+            dao.add(anexo);
+        } catch (AnexoDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Anexo get(long id) {
+        Anexo anexo = null;
+        try {
+            anexo = get(id);
+        } catch (AnexoDAOException e) {
+            e.printStackTrace();
+        }
+        return anexo;
+    }
+
+    public List<Anexo> getByMensagem(Mensagem mensagem) {
+        List<Anexo> anexos = null;
+        try {
+            anexos = dao.getByMensagem(mensagem);
+        } catch (AnexoDAOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remove(Anexo anexo) {
+        
+    }
+
+
 }
