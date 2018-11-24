0ed83076f244f393f55596de0a961eb9140b0bdb
Nicolas <nicolas010256@gmail.com>
2018-11-24 01:19:13 -0200

    Update App

--- sad/src/main/java/sad/App.java
+++ sad/src/main/java/sad/App.java
@@ -1,20 +1,17 @@
 package sad;
 
-import javax.swing.UnsupportedLookAndFeelException;
+import boundary.Login;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
     
     public static void main(String[] args) {
-
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-					javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
+        launch(args);
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        new Login();
+	}
 }
